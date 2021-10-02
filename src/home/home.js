d9c069a2402d053839cf0028a71b7e361b121729
Min <63823931+MuteMini@users.noreply.github.com>
2021-10-01 21:01:51 -0400

    init page

---
+++ src/home/home.js
@@ -0,0 +1,13 @@
+import { Center, Text } from "@chakra-ui/react"
+
+function Home() {
+    return (
+        <Center h="200px">
+            <Text size="5xl">
+                brurhubruhruhb
+            </Text>
+        </Center>
+    );
+}
+
+export default Home;
