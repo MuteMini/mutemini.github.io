930cc92b4427bb5188c656a0a79986a791d86bdd
Min <63823931+MuteMini@users.noreply.github.com>
2021-10-23 19:18:38 -0400

    animation of front page complete

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,14 +1,60 @@
-import { Center, Text } from "@chakra-ui/react"
+import { 
+    Center,
+    VStack, 
+    Text, 
+    Heading } from "@chakra-ui/react"
+import { useEffect } from "react"
+import { motion, useAnimation } from "framer-motion"
+import "@fontsource/poppins/400.css"
+import "@fontsource/balsamiq-sans/400-italic.css"
+import Spacer from "../components/Spacer"
 
 function Index() {
+    const titleControls = useAnimation();
+    const spacerControls = useAnimation();
+    const subtitleControls = useAnimation();
+
+    async function sequence() {
+        await titleControls.start({ 
+            opacity: 1,
+            translateY: 0,
+            transition: { duration: 1 },
+        });
+        await spacerControls.start({ 
+            pathLength: 1,
+            transition: { duration: 4, ease: "easeInOut" },
+        });
+        return await Promise.all([
+            spacerControls.start({ 
+                fillOpacity: 1,
+                transition: { duration: 1 },
+            }),
+            subtitleControls.start({ 
+                opacity: 1,
+                translateY: 0,
+                transition: { duration: 1, ease: "easeInOut" }
+            })
+        ]);
+    }
+
+    useEffect(() => {
+        sequence();
+    }, []);
+
     return (
         <Center 
-            background="palette.600"
+            bg="palette.600"
             minH="100vh" 
             h="100vh" >
-            <Text size="5xl">
-                brurhubruhruhb
-            </Text>
+            <VStack spacing="-10">
+                <motion.div initial={{ opacity: 0, translateY: "-50%" }} animate={titleControls} >
+                    <Heading margin="6" color="palette.900" size="4xl"> Hello! My name is Min! </Heading>
+                </motion.div>
+                <Spacer controls={spacerControls} />  
+                <motion.div initial={{ opacity: 0, translateY: "50%" }} animate={subtitleControls} >
+                    <Text color="palette.900" fontSize="3xl"> Welcome to my portfolio. </Text>
+                </motion.div>
+            </VStack>
         </Center>
     );
 }
