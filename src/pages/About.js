27180dfd0d549c899c9f35907c1e6180309bf167
Min <63823931+MuteMini@users.noreply.github.com>
2021-10-23 23:21:30 -0400

    Create About.js

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import { Center } from "@chakra-ui/react"
+
+function About() {
+    return(
+        <Center 
+            bg="palette.600"
+            minH="100vh" 
+            h="100vh" >
+        </Center>
+    );
+}
+
+export default About;
