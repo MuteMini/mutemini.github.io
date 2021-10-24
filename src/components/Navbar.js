95694b3c82d42c938ead9b0aa0e0e35cd4190ca8
Min <63823931+MuteMini@users.noreply.github.com>
2021-10-23 23:26:30 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,31 @@
-import { } from "@chakra-ui/react"
+import { HStack, Link } from "@chakra-ui/react"
+import { Link as RouterLink } from 'react-router-dom';
+
+function CustomLink(props) {
+    return (
+        <Link 
+            to={props.link}
+            as={RouterLink}
+            textDecoration="none"
+            color="palette.900"
+            textShadow="2px 2px 3px var(--chakra-colors-palette-700)"
+            _hover={{
+                color:"palette.800"
+            }}
+            transition="color 0.3s ease"
+            fontSize="2xl">
+            {props.children}
+        </Link>
+    );
+}
 
 function NavBar() {
     return (
-        
+        <HStack borderSize={1} padding={2} spacing={7}>
+            <CustomLink link="/about"> About Me </CustomLink>
+            <CustomLink link="/project"> Projects </CustomLink>
+            <CustomLink link="/contact"> Contacts </CustomLink>
+        </HStack>
     );
 }
 
