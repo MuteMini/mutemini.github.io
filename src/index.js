85659fa358a2c874185efaae5bb040021a4471bb
Min <63823931+MuteMini@users.noreply.github.com>
2021-10-31 18:57:52 -0400

    apparently this causes a bug? man...

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { hydrate, render } from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 
 const rootElement = document.getElementById('root');
 
