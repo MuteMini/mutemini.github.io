ce6474df8343db7469f4384c09698d1cb52c907d
Min <63823931+MuteMini@users.noreply.github.com>
2021-11-01 00:29:52 -0400

    Update About.jsx

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,21 +2,41 @@ import { Center,
     VStack, 
     Text, 
     Heading,
+    Container,
     Box } from "@chakra-ui/react"
 import { useEffect, lazy } from "react";
 import { useInView } from "react-intersection-observer";
 import { motion, useAnimation } from "framer-motion";
 import "@fontsource/balsamiq-sans/400-italic.css"
 import "@fontsource/poppins/400.css"
+
 import bgImage from './aboutBg.jpg';
+const Navbar = lazy(() => import('../../components/Navbar'));
 
-const Navbar = lazy(() => {
-    return new Promise(resolve => {
-        setTimeout(() => resolve(import('../../components/Navbar')));
-    });
-});
+const imgCache = {
+    __cache: {},
+    read(src) {
+        if (!this.__cache[src]) {
+            this.__cache[src] = new Promise((resolve) => {
+                const img = new Image();
+                img.onload = () => {
+                    this.__cache[src] = true;
+                    resolve(this.__cache[src]);
+                };
+                img.src = src;
+            }).then((img) => {
+                this.__cache[src] = true;
+            });
+        }
 
-function FadeInView({ children, duration }) {
+        if (this.__cache[src] instanceof Promise) {
+            throw this.__cache[src];
+        }
+        return this.__cache[src];
+    }
+};
+
+function FadeInView({ children, duration, distance }) {
     const control = useAnimation();
     const [ref, inView] = useInView();
 
@@ -32,39 +52,98 @@ function FadeInView({ children, duration }) {
         ref={ref}
         animate={control}
         initial={"hidden"}
-        transition={{type: "spring", duration: duration}}
+        transition={{duration: duration, ease: "easeInOut"}}
         variants={{
-            visible: { opacity: 1, x: 0 },
-            hidden: { opacity: 0, x: 100 }
+            visible: { opacity: 1, y: 0 },
+            hidden: { opacity: 0, y: distance }
         }}>
             {children}
         </motion.div>
     );
 }
+FadeInView.defaultProps = {
+    distance: "100px"
+};
+
+function InnerText({ children, maxW, ...rest }){
+    return (
+        <Container maxW={maxW} {...rest}>
+            <Text textAlign="center" h="inherit" fontSize="3xl" variant="sm">
+                {children}
+            </Text>
+        </Container>
+    );
+};
+InnerText.defaultProps = {
+    maxW: "60rem"
+}
+
+const Divider = () => (
+    <Center>
+        <Box w="50%" borderRadius="2px" border="2px solid" borderColor="palette.500"/>
+    </Center> 
+)
 
 function About() {
+    imgCache.read(bgImage);
+
     return(
         <>
             <Center flex="0 0" position="sticky" zIndex={1} top={0} w="100%" bg="palette.700">
                 <Navbar mainpage/>
             </Center>
-            <Box display="flex">
-                <Center flex="0 0 auto" h="100vh" w="100%" backgroundImage={bgImage}>
-                    <FadeInView duration={5}>
-                        <Heading fontSize="5xl">
-                            Welcome to my portfolio. 
+            <Center h="100vh" w="100%" 
+                backgroundImage={bgImage} 
+                backgroundSize="cover"
+                backgroundPosition="center right">
+                <Container maxW="50rem">
+                    <FadeInView duration={2}>
+                        <Heading textAlign="center" fontSize="6xl" variant="light">
+                            Who is Min?
+                        </Heading>
+                        <Heading textAlign="center" fontSize="3xl" variant="light-sm">
+                            I think I'm qualified to answer that... 😀
                         </Heading>
                     </FadeInView>
-                </Center>
-            </Box>
-            <Box display="flex">
-
-                <FadeInView duration={5}>
-                    <Text flex="0 0 auto" h="200px" fontSize="4xl">
-                        lets check if this is a penis!!!
-                    </Text>
+                </Container>
+            </Center>
+            <VStack h="100%" 
+                maxH="1400px" 
+                overflow="hidden" 
+                justifyContent="start"
+                margin={6}
+                spacing="100px">
+                <FadeInView duration={1} distance="30px">
+                    <InnerText marginTop="20px">
+                        Min is a grade 12 student currently attending 
+                        William Lyon Mackenzie C.I.
+                    </InnerText>
                 </FadeInView>
-            </Box>
+                <FadeInView duration={1} distance="30px">
+                    <InnerText>
+                        <Text fontSize="5xl"> Interests </Text>
+                        <Divider />
+                        Min is a puzzle addict, passionate video editor, 
+                        a novice trumpet and not so novice piano player, 
+                        as well as a budding programmer.
+                    </InnerText>
+                    <InnerText marginTop="50px">
+                        Min doesn't play much sports, but if he had to
+                        choose, his favouries would be ultimate frisbee,
+                        kendo, and soccer.
+                    </InnerText>
+                    <InnerText marginTop="50px">
+                        Min also loves to take photos. Here are some he took
+                        that he is proud of.
+                    </InnerText>
+                </FadeInView>
+                <FadeInView duration={1} distance="30px">
+                    <InnerText>
+                        <Text fontSize="5xl"> Skillz </Text>
+                        <Divider />
+                    </InnerText>
+                </FadeInView>
+            </VStack>
         </>
     );
 }
