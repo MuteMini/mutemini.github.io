8a0a2a17e318f34c6fb158397adf5e8e05021fc8
Min <63823931+MuteMini@users.noreply.github.com>
2021-11-05 16:02:27 -0400

    Create Project.jsx

---
+++ src/pages/project/Project.jsx
@@ -0,0 +1,21 @@
+import { Center } from "@chakra-ui/react"
+import { lazy } from "react";
+import "@fontsource/balsamiq-sans/400-italic.css"
+import "@fontsource/poppins/400.css"
+
+const Navbar = lazy(() => import('../../components/Navbar'));
+
+function Project() {
+    return (
+        <>
+            <Center position="sticky" zIndex={1} top={0} w="100%" bg="palette.700">
+                <Navbar mainpage/>
+            </Center>
+            <Center h="100vh" w="100%" >
+                
+            </Center>
+        </>
+    );
+}
+
+export default Project;
