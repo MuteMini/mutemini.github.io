273d2970201bc44ece48ce6ac84d037126d27253
Min <63823931+MuteMini@users.noreply.github.com>
2022-07-01 00:36:40 -0400

    better skillbar

--- src/components/SkillBar.jsx
+++ src/components/SkillBar.jsx
@@ -1,43 +1,71 @@
 import { Box, Text, Flex } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
+
+const MotionBox = motion(Box);
 
 class Color {
-    constructor(red, green, blue) {
-        this.red = parseInt(red);
-        this.green = parseInt(green);
-        this.blue = parseInt(blue);
+    constructor({hexCode, red, green, blue}) {
+        var codeInc = (hexCode !== undefined);
+        this.red = (codeInc) ? parseInt(hexCode.substr(1,2), 16) : parseInt(red);
+        this.green = (codeInc) ? parseInt(hexCode.substr(3,2), 16) : parseInt(green);
+        this.blue = (codeInc) ? parseInt(hexCode.substr(5,2), 16) : parseInt(blue);
     }
+
     getHexCode() {
         return "#"
-            +this.red.toString(16)
-            +this.green.toString(16)
-            +this.blue.toString(16);
+            +('00'+this.red.toString(16).toUpperCase()).slice(-2)
+            +('00'+this.green.toString(16).toUpperCase()).slice(-2)
+            +('00'+this.blue.toString(16).toUpperCase()).slice(-2);
     }
 }
 
-const start = new Color(236, 49, 69);
-const end = new Color(66, 141, 88);
+const start = new Color({hexCode:"#12c2e9"});
+const mid = new Color({hexCode:"#c471ed"});
+const end = new Color({hexCode:"#f64f59"});
 
-function interpolateColor(percent) {
-    var diffR = end.red - start.red;
-    var diffG = end.green - start.green;
-    var diffB = end.blue - start.blue;
+console.log(start.getHexCode())
 
-    percent = 1/(1+Math.exp(-10*(percent-0.4)));
+const interpolateColor = (percent) => {
+    percent = 1/(0.5+Math.exp(-10*(percent-0.435)));
+    var cstart, cend;
+    if(percent < 1) {
+        cstart = start;
+        cend = mid;
+    }
+    else {
+        cstart = mid;
+        cend = end;
+        percent -= 1;
+    }
 
-    diffR = (diffR * percent) + start.red;
-    diffG = (diffG * percent) + start.green;
-    diffB = (diffB * percent) + start.blue;
+    var diffR = cend.red - cstart.red;
+    var diffG = cend.green - cstart.green;
+    var diffB = cend.blue - cstart.blue;
 
-    return new Color(diffR, diffG, diffB);
-}
+    diffR = (diffR * percent) + cstart.red;
+    diffG = (diffG * percent) + cstart.green;
+    diffB = (diffB * percent) + cstart.blue;
+
+    return new Color({red:diffR, green:diffG, blue:diffB});
+};
 
 function SkillBar({ percent, children }) {
-    const [mainColor, setMainColor] = useState(null);
+    const control = useAnimation();
+    const [ref, inView] = useInView();
+
+    const [bgColor, setColor] = useState(null);
     const [title, setTitle] = useState(null);
 
     useEffect(() => {
-        setMainColor( interpolateColor(percent).getHexCode() );
+        if (inView) {
+            control.start({"--animate-width": 1});
+        }
+    }, [control, inView]);
+
+    useEffect(() => {
+        setColor( interpolateColor(percent).getHexCode() );
         if(percent <= 0.4) {
             setTitle("Beginner");
         } else if(percent <= 0.6) {
@@ -52,9 +80,17 @@ function SkillBar({ percent, children }) {
     return (
         <Flex alignItems="center">
             <Box h="35px" w={{base:"50vw", md:"500px"}} borderRadius="xl" bg="gray.400">
-                <Box h="100%" w={"calc(100%*"+percent+")"} borderRadius="xl" bg={mainColor}>
+                <MotionBox 
+                    ref={ref}
+                    animate={control}
+                    transition={{ duration: 2, ease: "easeOut" }}
+                    initial={{"--animate-width": 0}}
+                    h="100%" 
+                    w={"calc(100%*"+percent+"*var(--animate-width))"} 
+                    borderRadius="xl" 
+                    bg={bgColor}>
                     <Text position="relative" top="3px" left="10px" fontSize="lg" whiteSpace="nowrap" variant="light-sm"> {title} </Text>
-                </Box>
+                </MotionBox>
             </Box>
             {children}
         </Flex>
