c5171f3d54d4c519bbd63c18ef49a239d0587d04
Min <63823931+MuteMini@users.noreply.github.com>
2022-07-01 00:36:22 -0400

    Create Contact.jsx

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,21 @@
+import { lazy } from "react";
+import { Center } from "@chakra-ui/react"
+import "@fontsource/balsamiq-sans/400-italic.css"
+import "@fontsource/poppins/400.css"
+
+const Navbar = lazy(() => import('../components/Navbar'));
+
+function Contact() {
+    return (
+        <>
+            <Center position="sticky" zIndex={1} top={0} w="100%" bg="palette.700">
+                <Navbar mainpage/>
+            </Center>
+            <Center h="100vh" w="100%" >
+                
+            </Center>
+        </>
+    );
+}
+
+export default Contact;
