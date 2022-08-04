32941d0cdcd9382c3aeb992a653e17ea13b62d1f
Min <63823931+MuteMini@users.noreply.github.com>
2022-08-04 19:05:11 -0400

    formatting

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,11 @@
 import {
     Box, Center, Container, Flex, Heading, Text, VStack
-} from "@chakra-ui/react"
-import "@fontsource/balsamiq-sans/400-italic.css"
-import "@fontsource/poppins/400.css"
-import { motion, useAnimation } from "framer-motion"
-import { lazy, useEffect } from "react"
-import { useInView } from "react-intersection-observer"
+} from '@chakra-ui/react'
+import '@fontsource/balsamiq-sans/400-italic.css'
+import '@fontsource/poppins/400.css'
+import { motion, useAnimation } from 'framer-motion'
+import { lazy, useEffect } from 'react'
+import { useInView } from 'react-intersection-observer'
 import FadeInView from '../containers/FadeInView'
 import imgCache from '../imgCache'
 
@@ -21,7 +21,7 @@ function Divider() {
 
     useEffect(() => {
         if (inView) {
-            control.start("visible");
+            control.start('visible');
         }
     }, [control, inView]);
 
@@ -29,16 +29,16 @@ function Divider() {
         <Center>
             <MotionBox
                 zIndex={0}
-                borderRadius="2px"
-                border="2px solid"
-                borderColor="palette.500"
+                borderRadius='2px'
+                border='2px solid'
+                borderColor='palette.500'
                 ref={ref}
                 animate={control}
-                initial={"hidden"}
-                transition={{ duration: 2, ease: "easeOut" }}
+                initial={'hidden'}
+                transition={{ duration: 2, ease: 'easeOut' }}
                 variants={{
-                    visible: { width: "50%" },
-                    hidden: { width: "0%" }
+                    visible: { width: '50%' },
+                    hidden: { width: '0%' }
                 }} 
             />
         </Center> 
@@ -48,91 +48,91 @@ function Divider() {
 function InnerText({ children, maxW, ...rest }) {
     return (
         <Container maxW={maxW} {...rest}>
-            <Text textAlign="center" h="inherit" fontSize={{base: "xl", md: "2xl", lg: "3xl"}} variant="sm">
+            <Text textAlign='center' h='inherit' fontSize={{base: 'xl', md: '2xl', lg: '3xl'}} variant='sm'>
                 {children}
             </Text>
         </Container>
     );
 };
 InnerText.defaultProps = {
-    maxW: "60rem"
+    maxW: '60rem'
 }
 
 function About() {
     imgCache.read(process.env.PUBLIC_URL+'/img/aboutBg.jpg');
 
     const skillInfo = [
-        {name:"Java | 2 years", p:0.6},
-        {name:"Python | 6 months", p:0.35},
-        {name:"C++ | 2 years", p:0.7},
-        {name:"Web Dev | 1 year", p:0.5},
+        {name:'Java | 2 years', p:0.6},
+        {name:'Python | 6 months', p:0.35},
+        {name:'C++ | 2 years', p:0.7},
+        {name:'Web Dev | 1 year', p:0.5},
     ];
 
     return(
         <>
-            <Center position="sticky" zIndex={1} top={0} bg="palette.700">
+            <Center position='sticky' zIndex={1} top={0} bg='palette.700'>
                 <Navbar mainpage/>
             </Center>
-            <Flex direction="column" h="100vh">
+            <Flex direction='column' h='100vh'>
                 <Center 
-                    flex="1 1 auto"
-                    backgroundImage={process.env.PUBLIC_URL+"/img/aboutBg.jpg"} 
-                    backgroundSize="cover"
-                    backgroundAttachment="fixed"
-                    backgroundPosition="center right"
+                    flex='1 1 auto'
+                    backgroundImage={process.env.PUBLIC_URL+'/img/aboutBg.jpg'} 
+                    backgroundSize='cover'
+                    backgroundAttachment='fixed'
+                    backgroundPosition='center right'
                 >
-                    <Container maxW="50rem">
+                    <Container mt='-10px' maxW='50rem'>
                         <FadeInView duration={2}>
-                            <Heading textAlign="center" fontSize="6xl" variant="light">
+                            <Heading textAlign='center' fontSize='6xl' variant='light'>
                                 Who is Min?
                             </Heading>
-                            <Heading textAlign="center" fontSize="3xl" variant="light-sm">
-                                I think I'm qualified to answer that... <Text style={{"-webkit-text-stroke": "0"}}>😀</Text>
+                            <Heading textAlign='center' fontSize='3xl' variant='light-sm'>
+                                I think I'm qualified to answer that... <Text style={{'-webkit-text-stroke': '0'}}>😀</Text>
                             </Heading>
                         </FadeInView>
                     </Container>
                 </Center>
             </Flex>
             <VStack 
-                h="100%" 
-                maxH="3000px" 
-                overflow="hidden" 
-                justifyContent="start"
+                h='100%' 
+                maxH='3000px' 
+                overflow='hidden' 
+                justifyContent='start'
                 margin={6}
-                spacing={{base:"40px", lg:"70px"}}
+                spacing={{base:'40px', lg:'70px'}}
             >
-                <FadeInView duration={1} distance="30px">
-                    <InnerText marginTop="20px">
+                <FadeInView duration={1} distance='30px'>
+                    <InnerText marginTop='20px'>
                         Min is a first year Waterloo ECE student. <br/>
                         He is also an alumni of the MACS program.
                     </InnerText>
                 </FadeInView>
-                <FadeInView duration={1} distance="30px">
+                <FadeInView duration={1} distance='30px'>
                     <InnerText>
-                        <Text fontSize={{base: "3xl", lg: "5xl"}}> Interests </Text>
+                        <Text fontSize={{base: '3xl', lg: '5xl'}}> Interests </Text>
                         <Divider />
                         Min is a puzzle addict, passionate video editor, 
                         a novice trumpet and not so novice piano player, 
                         as well as a budding programmer.
                     </InnerText>
-                    <InnerText marginTop="50px">
+                    <InnerText marginTop='50px'>
                         Min doesn't play much sports, but if he had to
                         choose, his favouries would be ultimate frisbee,
                         kendo, and rock climbing.
                     </InnerText>
-                    <InnerText marginTop="50px">
+                    <InnerText marginTop='50px'>
                         He also loves to take photos.
                     </InnerText>
                     <Carousel />
                 </FadeInView>
-                <FadeInView duration={1} distance="30px">
+                <FadeInView duration={1} distance='30px'>
                     <InnerText>
-                        <Text fontSize="5xl"> Skillz </Text>
+                        <Text fontSize={{base: '3xl', lg: '5xl'}}> Skillz </Text>
                         <Divider />
                     </InnerText>
                     {skillInfo.map((skill, sid) => (
                         <SkillBar percent={skill.p}>
-                            <InnerText m="5px" maxW="20rem"> {skill.name} </InnerText>
+                            <InnerText m='5px' maxW='20rem'> {skill.name} </InnerText>
                         </SkillBar>
                     ))}
                 </FadeInView>
