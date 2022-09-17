6b47b4432888bbdda345c34df7c50d3717aead83
Min <63823931+MuteMini@users.noreply.github.com>
2022-09-17 15:24:00 -0400

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,34 @@
+import { Center, Container, Heading } from '@chakra-ui/react';
+import '@fontsource/balsamiq-sans/400-italic.css';
+import '@fontsource/poppins/400.css';
+import { lazy } from 'react';
+import FadeInView from '../containers/FadeInView';
+
+const Navbar = lazy(() => import('../components/Navbar'));
+
 function ErrorPage() {
-    return <></>
+    return (
+    <>
+        <Center position='sticky' zIndex={1} top={0} w='100%' bg='palette.700'>
+            <Navbar mainpage/>
+        </Center>
+        <Center
+            h='90vh'
+            backgroundColor='palette.300'
+            backgroundSize='cover'
+            backgroundAttachment='fixed'
+            backgroundPosition='center'
+        >
+            <Container mt='-80px' maxW='50rem'>
+                <FadeInView duration={2}>
+                    <Heading textAlign='center' fontSize='5xl' variant='light'>
+                        404 <br/> This link doesn't exist :(
+                    </Heading>
+                </FadeInView>
+            </Container>
+        </Center>
+    </>
+    );
 }
 
 export default ErrorPage;
